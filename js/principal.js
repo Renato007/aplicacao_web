cebe93bfbfb732113dac88cce7fdb3d666e78eb7
Renato <renatosilsoa@gmail.com>
2022-03-02 21:02:17 -0300

    adição da pasta js

---
+++ js/principal.js
@@ -0,0 +1,5 @@
+console.log("olá mundo");
+console.log(document.querySelector("h1"));
+var titulo = document.querySelector(".titulo");
+console.log(titulo.textContent);
+//titulo.textContent = "carregado por um arquivo externo";
