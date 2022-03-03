03edd3b25abeeec7b8dde5bd8a88b9a086b80e45
Renato <renatosilsoa@gmail.com>
2022-03-03 20:41:04 -0300

    [fenture]: calculo do IMC do Paulo

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,25 @@
-console.log("olá mundo");
-console.log(document.querySelector("h1"));
 var titulo = document.querySelector(".titulo");
-console.log(titulo.textContent);
-//titulo.textContent = "carregado por um arquivo externo";
+titulo.textContent = "Aparecida Nutricionista "
+
+var paciente = document.querySelector("#primeiro_paciente")
+var tdPeso= paciente.querySelector(".info-peso");
+
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura")
+
+var altura = tdAltura.textContent;
+
+var td_IMC = paciente.querySelector(".info-imc");
+
+var calculoIMC = peso/(altura*altura);
+ 
+td_IMC.textContent = calculoIMC;
+
+
+console.log(paciente) // tr 
+console.log(peso) // obter o valor do peso
+console.log(altura) // obter o valor da altura
+console.log(calculoIMC) // obter o resultado do calculo
+
+
