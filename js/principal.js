f8d9c9e906f7c6fe6d48998e66dbba1c34703f04
Renato Soares <renatosilsoa@gmail.com>
2022-04-13 22:28:23 -0300

    adicionar valores atraves de inputs para a tabela

--- js/principal.js
+++ js/principal.js
@@ -42,4 +42,45 @@ for (var i = 0; i < pacientes.length; i++) {
 
         td_IMC.textContent = calculoIMC.toFixed(2);//limita a casa decimais
     }
-};
+}
+var botaoAdicionar = document.querySelector("#adicionar-paciente")
+
+botaoAdicionar.addEventListener("click", function(event){
+ event.preventDefault();// segura o evento padrão
+
+ //armazenar valores do mundo HTML no mundo javascript
+ //atributos name e value.
+ var form = document.querySelector("#form-adiciona");
+
+var nome = form.nome.value;
+var peso = form.peso.value;
+var altura = form.altura.value;
+var gordura = form.gordura.value;
+//
+
+// criar uma linha de paciente
+var pacienteTr = document.createElement("tr");
+
+//criando os dados
+var nomeTd = document.createElement("td");
+var alturaTd = document.createElement("td");
+var pesoTd = document.createElement("td");
+var gorduraTd = document.createElement("td");
+var imcTd = document.createElement("td");
+
+
+//atribuindo o dado armazenado atraves do imput para o conteudo de texto da tag dado de tabela.
+nomeTd.textContent = nome;
+pesoTd.textContent = peso;
+alturaTd.textContent = altura;
+gorduraTd.textContent = gordura;
+
+//adicionar filhos no elemento pai - coloque como filho
+pacienteTr.appendChild(nomeTd);
+pacienteTr.appendChild(pesoTd);
+pacienteTr.appendChild(alturaTd);
+pacienteTr.appendChild(gorduraTd);
+
+var tabela = document.querySelector("#tabela-pacientes");
+tabela.appendChild(pacienteTr)
+})
